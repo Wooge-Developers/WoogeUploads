73926f4dbdf2820d98ff6e0f1d0d7091cb0aa15a
Hedy <43907004+Hedy88@users.noreply.github.com>
2022-01-08 10:01:58 +0000

    c
    c

---
+++ api/uploadFile.php
@@ -0,0 +1,42 @@
+<?php
+    $currentDirectory = getcwd();
+    $uploadDirectory = "/uploads/";
+
+    $errors = []; 
+
+    $fileExtensionsAllowed = ['jpeg','jpg','png','mp4','bmp','gif','webm','txt','sh']; // These will be the only file extensions allowed 
+
+    $fileName = $_FILES['the_file']['name'];
+    $fileSize = $_FILES['the_file']['size'];
+    $fileTmpName  = $_FILES['the_file']['tmp_name'];
+    $fileType = $_FILES['the_file']['type'];
+    $fileExtension = strtolower(end(explode('.',$fileName)));
+
+    $uploadPath = $currentDirectory . $uploadDirectory . basename($fileName); 
+
+    if (isset($_POST['submit'])) {
+
+      if (! in_array($fileExtension,$fileExtensionsAllowed)) {
+        $errors[] = "This file extension is not allowed. Please check the FAQ for the list of allowed file extensions.";
+      }
+
+      if ($fileSize > 4000000) {
+        $errors[] = "File exceeds maximum size (4MB)";
+      }
+
+      if (empty($errors)) {
+        $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
+
+        if ($didUpload) {
+          echo "The file " . basename($fileName) . " has been uploaded Check Your File at /api/uploads/YOUR FILENAME";
+        } else {
+          echo "An error occurred. Please contact the administrator.";
+        }
+      } else {
+        foreach ($errors as $error) {
+          echo $error . "These are the errors" . "\n";
+        }
+      }
+
+    }
+?>
